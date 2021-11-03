f2dcc01912240b74b73ed3ef1cadec00bb903432
Srikanth Macha <srikanthmacha32@gmail.com>
2021-11-03 21:56:41 +0530

    Optimization : Used Singleton Class

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this ,Second_Activity.class);
+            startActivity(intent);
+            MainActivity.this.finish();
+        },900);
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Second_Activity.java
@@ -0,0 +1,157 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.LruCache;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import org.json.JSONException;
+
+public class Second_Activity extends AppCompatActivity {
+
+    private String urlName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        loadMeme();
+
+        Button Next = findViewById(R.id.NextbuttonID);
+        Button share = findViewById(R.id.SharebuttonID);
+
+        Next.setOnClickListener(v ->
+                loadMeme());
+
+        share.setOnClickListener(v -> {
+
+            Intent intent = new Intent(Intent.ACTION_SEND).setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT,urlName);
+            Intent chooser = Intent.createChooser(intent,"Share this using");
+
+            startActivity(chooser);
+        });
+
+    }
+
+    void loadMeme(){
+
+        String url = "https://meme-api.herokuapp.com/gimme";
+
+        ProgressBar progressBar = findViewById(R.id.progressbarID);
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                url,
+                null,
+                response -> {
+                    try {
+                         urlName = response.getString("url");
+
+                         ImageView imageView = findViewById(R.id.imageViewID);
+
+                         Glide.with(this).load(urlName).listener(new RequestListener<Drawable>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e,
+                                                        Object model, Target<Drawable> target, boolean isFirstResource) {
+
+                                Toast.makeText(Second_Activity.this,"Something went wrong",Toast.LENGTH_LONG).show();
+
+                                progressBar.setVisibility(View.GONE);
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                progressBar.setVisibility(View.GONE);
+                                return false;
+                            }
+                         }).into(imageView);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> Toast.makeText(Second_Activity.this,"Error Occurred",Toast.LENGTH_LONG).show());
+
+        MySingleton.getInstance(Second_Activity.this).addToRequestQueue(jsonObjectRequest);
+    }
+
+}
+
+
+
+class MySingleton {
+    private static MySingleton instance;
+    private RequestQueue requestQueue;
+    private final ImageLoader imageLoader;
+    private final Context ctx;
+
+    private MySingleton(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
+
+        imageLoader = new ImageLoader(requestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized MySingleton getInstance(Context context) {
+        if (instance == null) {
+            instance = new MySingleton(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
+}
