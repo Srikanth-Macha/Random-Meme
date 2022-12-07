df8b191004030f1a0d829a94b8960a5dc1388daa
Srikanth Macha <srikanthmacha32@gmail.com>
2022-12-07 16:02:56 +0530

    changed API URL

--- app/src/androidTest/java/com/example/RandomMemes/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/RandomMemes/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.RandomMemes;
 
 import android.content.Context;
 
--- app/src/main/java/com/example/RandomMemes/Second_Activity.java
+++ app/src/main/java/com/example/RandomMemes/Second_Activity.java
@@ -1,11 +1,9 @@
-package com.example.myapplication;
+package com.example.RandomMemes;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.LruCache;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,7 +15,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
@@ -42,22 +39,21 @@ public class Second_Activity extends AppCompatActivity {
         Button Next = findViewById(R.id.NextbuttonID);
         Button share = findViewById(R.id.SharebuttonID);
 
-        Next.setOnClickListener(v ->
-                loadMeme());
+        Next.setOnClickListener(v -> loadMeme());
 
         share.setOnClickListener(v -> {
 
             Intent intent = new Intent(Intent.ACTION_SEND).setType("text/plain");
-            intent.putExtra(Intent.EXTRA_TEXT,urlName);
-            Intent chooser = Intent.createChooser(intent,"Share this using");
+            intent.putExtra(Intent.EXTRA_TEXT, urlName);
+            Intent chooser = Intent.createChooser(intent, "Share this using");
 
             startActivity(chooser);
         });
 
     }
 
-    void loadMeme(){
-        String url = "https://meme-api.herokuapp.com/gimme";
+    void loadMeme() {
+        String url = "https://meme-api.com/gimme";
 
         ProgressBar progressBar = findViewById(R.id.progressbarID);
 
@@ -68,66 +64,49 @@ public class Second_Activity extends AppCompatActivity {
                 null,
                 response -> {
                     try {
-                         urlName = response.getString("url");
+                        urlName = response.getString("url");
 
-                         ImageView imageView = findViewById(R.id.imageViewID);
+                        ImageView imageView = findViewById(R.id.imageViewID);
 
-                         Glide.with(this).load(urlName).listener(new RequestListener<Drawable>() {
+                        Glide.with(this).load(urlName).listener(new RequestListener<Drawable>() {
                             @Override
                             public boolean onLoadFailed(@Nullable GlideException e,
                                                         Object model, Target<Drawable> target, boolean isFirstResource) {
 
-                                Toast.makeText(Second_Activity.this,"Something went wrong",Toast.LENGTH_LONG).show();
+                                Toast.makeText(Second_Activity.this, "Something went wrong", Toast.LENGTH_LONG).show();
 
                                 progressBar.setVisibility(View.GONE);
                                 return false;
                             }
 
                             @Override
-                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target,
+                                                           DataSource dataSource, boolean isFirstResource) {
                                 progressBar.setVisibility(View.GONE);
                                 return false;
                             }
-                         }).into(imageView);
+                        }).into(imageView);
 
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 },
-                error -> Toast.makeText(Second_Activity.this,"Error Occurred",Toast.LENGTH_LONG).show());
+                error -> Toast.makeText(Second_Activity.this, "Error Occurred", Toast.LENGTH_LONG).show());
 
         MySingleton.getInstance(Second_Activity.this).addToRequestQueue(jsonObjectRequest);
     }
 
 }
 
-
-
 class MySingleton {
     private static MySingleton instance;
-    private RequestQueue requestQueue;
-    private final ImageLoader imageLoader;
+    //    private final ImageLoader imageLoader;
     private final Context ctx;
+    private RequestQueue requestQueue;
 
     private MySingleton(Context context) {
         ctx = context;
         requestQueue = getRequestQueue();
-
-        imageLoader = new ImageLoader(requestQueue,
-                new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap>
-                            cache = new LruCache<>(20);
-
-                    @Override
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
-
-                    @Override
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
-                    }
-                });
     }
 
     public static synchronized MySingleton getInstance(Context context) {
@@ -150,7 +129,4 @@ class MySingleton {
         getRequestQueue().add(req);
     }
 
-    public ImageLoader getImageLoader() {
-        return imageLoader;
-    }
 }
